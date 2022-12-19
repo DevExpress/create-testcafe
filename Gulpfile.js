37132e2eefefdbd6433ac762f68ec982992935ba
artem-babich <artem.babich@devexpress.com>
2022-12-19 22:17:33 +0400

    add GitHub workflow

--- Gulpfile.js
+++ Gulpfile.js
@@ -21,9 +21,21 @@ gulp.task('clean', () => {
     return del('dist');
 });
 
-gulp.task('compile', () => {
-    return childProcess.spawn('tsc', { shell: true, stdio: 'inherit' });
+gulp.task('compile', () => childProcess.spawn('tsc', { shell: true, stdio: 'inherit' }));
+
+gulp.task('reinstall-package', () => {
+    childProcess.spawn('npm uninstall -g create-testcafe', { shell: true, stdio: 'inherit' });
+    childProcess.spawn('npm i -g', { shell: true, stdio: 'inherit' });
+});
+
+gulp.task('test-mocha', () => {
+    const mochaOpts = [
+        'test/**/*test.js',
+    ];
+
+    return childProcess.spawn(`npx mocha ${ mochaOpts.join(' ') }`, { stdio: 'inherit', shell: true });
 });
 
 gulp.task('fast-build', gulp.series('clean', 'compile'));
 gulp.task('build', gulp.parallel('lint', 'fast-build'));
+gulp.task('test', gulp.series('build', 'reinstall-package', 'test-mocha'));
